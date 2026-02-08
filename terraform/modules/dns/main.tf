6e008651baedc3d8baa1401452017507ef82bdee
borninthedark <info@princetonstrong.online>
2026-02-07 22:35:19 -0500

    hcp tf #43.6

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -51,18 +51,11 @@ resource "aws_route53_record" "app" {
 # Proton Mail
 # ================================================================
 
-# Migrate the old combined TXT record to the new SPF-only resource.
-# Remove this block after the first successful apply.
-moved {
-  from = aws_route53_record.txt
-  to   = aws_route53_record.spf
-}
-
 resource "aws_route53_record" "spf" {
   zone_id = var.hosted_zone_id
   name    = ""
   type    = "TXT"
-  ttl     = 3600
+  ttl     = 300
 
   multivalue_answer_routing_policy = true
   set_identifier                   = "spf"
@@ -77,7 +70,7 @@ resource "aws_route53_record" "protonmail_verification" {
   zone_id = var.hosted_zone_id
   name    = ""
   type    = "TXT"
-  ttl     = 3600
+  ttl     = 300
 
   multivalue_answer_routing_policy = true
   set_identifier                   = "protonmail-verification"
